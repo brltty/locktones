a7335c0e4c36b3520782a1feb84db1edb74ee730
Dave Mielke <Dave@Mielke.cc>
2018-05-24 14:49:55 -0400

    Ensure that the current virtual console is always being monitored. (np)

--- lktLinux.c
+++ lktLinux.c
@@ -9,8 +9,10 @@
 
 /* Linux-specific includes. */
 #include <linux/kd.h>
+#include <linux/vt.h>
 
 static int console_descriptor = -1; // A handle to the console I/O subsystem.
+static int console_number = -1;     /* The foreground console number */
 
 /* Get a handle to the console I/O subsystem. */
 static void
@@ -44,6 +46,18 @@ int
 get_flags (void) {
    int flags = 0;
    unsigned char leds = 0;
+   struct vt_stat state;
+
+   if (console_descriptor < 0)
+      return 0;
+
+   if (ioctl(console_descriptor, VT_GETSTATE, &state) != -1 &&
+       state.v_active != console_number) {
+      /* Reopen the console to get to the foreground console */
+      close(console_descriptor);
+      open_console();
+      console_number = state.v_active;
+   }
 
    if (ioctl(console_descriptor, KDGKBLED, &leds) != -1) {
       /* The query of the lock states was successful. */
